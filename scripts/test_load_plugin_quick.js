47b0e46b0240bb18f6b3ba0e0c8f18f4d20ef77c
Tech-BH2 <jordan220@hotmail.com>
2025-09-21 05:26:48 +0000

    updated

--- scripts/test_load_plugin_quick.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const plugin = require(path.join('..','index.js'));
-class FakeCharacteristic { constructor(name, uuid) { this.displayName = name; this.UUID = uuid; } setProps(){} getDefaultValue(){return null;} }
-class FakeService { constructor(name){ this.displayName = name; } }
-const fakeApi = { hap: { Service: FakeService, Characteristic: FakeCharacteristic }, registerAccessory: (n,c)=>console.log('reg',n) };
-try { plugin(fakeApi); console.log('OK'); } catch(e) { console.error('ERR',e); process.exit(2); }
