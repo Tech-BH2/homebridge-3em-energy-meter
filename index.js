d1231aa7c45558aa02780c9be053bbb644e53dea
Tech-BH2 <jordan220@hotmail.com>
2025-09-21 00:50:33 +0000

    upgrad plugin 2.0 edits

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let Service, Characteristic, FakeGatoHistoryService;
 
 module.exports = (api) => {
   api.registerAccessory('3EMEnergyMeter', EnergyMeter);
-};
+
 
 
 function EnergyMeter(log, config, api) {
@@ -87,69 +87,20 @@ function EnergyMeter(log, config, api) {
 	EveVoltage1.UUID = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
 	inherits(EveVoltage1, Characteristic);
 
-	var EveAmpere1 = function () {
-		Characteristic.call(this, 'Ampere', 'E863F126-079E-48FF-8F27-9C2605A29F52');
-		this.setProps({
-			format: Characteristic.Formats.FLOAT,
-			unit: 'Ampere',
-			maxValue: 1000000000,
-			minValue: 0,
-			minStep: 0.001,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	EveAmpere1.UUID = 'E863F126-079E-48FF-8F27-9C2605A29F52';
-	inherits(EveAmpere1, Characteristic);
-
-	var PowerMeterService = function (displayName, subtype) {
-		Service.call(this, displayName, '00000001-0000-1777-8000-775D67EC4377', subtype);
-		this.addCharacteristic(EvePowerConsumption);
-		this.addOptionalCharacteristic(EveTotalConsumption);
-		this.addOptionalCharacteristic(EveVoltage1);
-		this.addOptionalCharacteristic(EveAmpere1);
-	};
-	PowerMeterService.UUID = '00000001-0000-1777-8000-775D67EC4377';
-	inherits(PowerMeterService, Service);
-
-	// local vars
-	this._EvePowerConsumption = EvePowerConsumption;
-	this._EveTotalConsumption = EveTotalConsumption;
-	this._EveVoltage1 = EveVoltage1;
-	this._EveAmpere1 = EveAmpere1;
-
-	// info
-	this.informationService = new Service.AccessoryInformation();
-	this.informationService
-		.setCharacteristic(Characteristic.Manufacturer, "Shelly - produdegr")
-		.setCharacteristic(Characteristic.Model, "Shelly 3EM")
-		.setCharacteristic(Characteristic.FirmwareRevision, version)
-		.setCharacteristic(Characteristic.SerialNumber, this.serial);
-
-	// construct service
-	this.service = new PowerMeterService(this.name);
-	this.service.getCharacteristic(this._EvePowerConsumption).on('get', this.getPowerConsumption.bind(this));
-	this.service.addCharacteristic(this._EveTotalConsumption).on('get', this.getTotalConsumption.bind(this));
-	this.service.addCharacteristic(this._EveVoltage1).on('get', this.getVoltage1.bind(this));
-	this.service.addCharacteristic(this._EveAmpere1).on('get', this.getAmpere1.bind(this));
-
-	// add fakegato
-	this.historyService = new FakeGatoHistoryService("energy", this, { storage: 'fs' });
-}
-
-EnergyMeter.prototype.updateState = function () {
-	if (this.waiting_response) {
-		this.log('Please select a higher update_interval value. Http command may not finish!');
-		return;
-	}
-	this.waiting_response = true;
-	this.last_value = new Promise((resolve, reject) => {
-		var ops = {
-			uri:		this.url,
-			method:		this.http_method,
-			timeout:	this.timeout
+	EnergyMeter.prototype.updateState = async function () {
+		if (this.waiting_response) {
+			this.log('Please select a higher update_interval value. Http command may not finish!');
+			return;
+		}
+		this.waiting_response = true;
+		const ops = {
+			uri: this.url,
+			method: this.http_method,
+			timeout: this.timeout
 		};
-		if (this.debug_log) { this.log('Requesting energy values from Shelly 3EM(EM) ...'); }
+		if (this.debug_log) {
+			this.log('Requesting energy values from Shelly 3EM(EM) ...');
+		}
 		if (this.auth) {
 			ops.auth = {
 				user: this.auth.user,
@@ -157,163 +108,122 @@ EnergyMeter.prototype.updateState = function () {
 			};
 		}
 		request(ops, (error, res, body) => {
-			var json = null;
 			if (error) {
 				this.log('Bad http response! (' + ops.uri + '): ' + error.message);
+				this.waiting_response = false;
+				return;
 			}
-			else {
-				try {
-					json = JSON.parse(body);
-					
-					if ((this.use_pf) && (this.use_em==false)) {
-						this.pf0 = parseFloat(json.emeters[0].pf);
-						this.pf1 = parseFloat(json.emeters[1].pf);
-						this.pf2 = parseFloat(json.emeters[2].pf);
-					}
-					else {
-						this.pf0 = 1;
-						this.pf1 = 1;
-						this.pf2 = 1;
-					}
-					
-					if (this.use_em) {
-						
-						if (this.use_em_mode == 0) {
-					   if (this.negative_handling_mode == 0) {
-					   	this.powerConsumption = (parseFloat(json.emeters[0].power)+parseFloat(json.emeters[1].power));
-					    this.totalPowerConsumption = ((parseFloat(json.emeters[0].total)+parseFloat(json.emeters[1].total))/1000);
-					    this.voltage1 = (((parseFloat(json.emeters[0].voltage)+parseFloat(json.emeters[1].voltage))/2));
-				    	this.ampere1 = ((this.powerConsumption/this.voltage1));
-				    	if (this.powerConsumption < 0) { this.powerConsumption = 0 }
-				    	if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0 }
-				    	if (this.voltage1 < 0) { this.voltage1 = 0 }
-				    	if (this.ampere1 < 0) { this.ampere1 = 0 }
-					  } else if (this.negative_handling_mode == 1) {
-					    this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power)+parseFloat(json.emeters[1].power));
-					    this.totalPowerConsumption = Math.abs((parseFloat(json.emeters[0].total)+parseFloat(json.emeters[1].total))/1000);
-					    this.voltage1 = Math.abs(((parseFloat(json.emeters[0].voltage)+parseFloat(json.emeters[1].voltage))/2));
-				    	    this.ampere1 = Math.abs((this.powerConsumption/this.voltage1));
-				    	}
-						  	
-					  } else
-					  	{ if (this.use_em_mode == 1) {
-					        if (this.negative_handling_mode == 0) {
-					        this.powerConsumption = (parseFloat(json.emeters[0].power));
-					        this.totalPowerConsumption = (parseFloat(json.emeters[0].total)/1000);
-					        this.voltage1 = (parseFloat(json.emeters[0].voltage));
-				        	this.ampere1 = ((this.powerConsumption/this.voltage1));
-				    	    if (this.powerConsumption < 0) { this.powerConsumption = 0 }
-				    	    if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0 }
-				    	    if (this.voltage1 < 0) { this.voltage1 = 0 }
-				    	    if (this.ampere1 < 0) { this.ampere1 = 0 }
-					        } else if (this.negative_handling_mode == 1) {
-					        this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power));
-					        this.totalPowerConsumption = Math.abs(parseFloat(json.emeters[0].total)/1000);
-					        this.voltage1 = Math.abs(parseFloat(json.emeters[0].voltage));
-				        	this.ampere1 = Math.abs((this.powerConsumption/this.voltage1));
-				      }
-						  	
-					    } else 
-					      { if (this.use_em_mode == 2) {
-					        if (this.negative_handling_mode == 0) {
-					        this.powerConsumption = (parseFloat(json.emeters[1].power));
-					        this.totalPowerConsumption = (parseFloat(json.emeters[1].total)/1000);
-					        this.voltage1 = (parseFloat(json.emeters[1].voltage));
-				          this.ampere1 = ((this.powerConsumption/this.voltage1));
-				    	    if (this.powerConsumption < 0) { this.powerConsumption = 0 }
-				    	    if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0 }
-				    	    if (this.voltage1 < 0) { this.voltage1 = 0 }
-				    	    if (this.ampere1 < 0) { this.ampere1 = 0 }
-					        } else if (this.negative_handling_mode == 1) {
-					          this.powerConsumption = Math.abs(parseFloat(json.emeters[1].power));
-					          this.totalPowerConsumption = Math.abs(parseFloat(json.emeters[1].total)/1000);
-					          this.voltage1 = Math.abs(parseFloat(json.emeters[1].voltage));
-				          	this.ampere1 = Math.abs((this.powerConsumption/this.voltage1));
-				          	}
-						  	
-					        }
-					        }	
-					  	} 
-						} 
-						else {
-						 			if (this.negative_handling_mode == 0) {
-				           this.powerConsumption = (parseFloat(json.emeters[0].power)+parseFloat(json.emeters[1].power)+parseFloat(json.emeters[2].power));
-				           this.totalPowerConsumption = ((parseFloat(json.emeters[0].total)+parseFloat(json.emeters[1].total)+parseFloat(json.emeters[2].total))/1000);
-			             this.voltage1 = (((parseFloat(json.emeters[0].voltage)+parseFloat(json.emeters[1].voltage)+parseFloat(json.emeters[2].voltage))/3));
-				           this.ampere1 = (((parseFloat(json.emeters[0].current)*this.pf0)
-					          +(parseFloat(json.emeters[1].current)*this.pf1)
-					          +(parseFloat(json.emeters[2].current)*this.pf2)));
-				    	    if (this.powerConsumption < 0) { this.powerConsumption = 0 }
-				    	    if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0 }
-				    	    if (this.voltage1 < 0) { this.voltage1 = 0 }
-				    	    if (this.ampere1 < 0) { this.ampere1 = 0 }
-					        } else if (this.negative_handling_mode == 1) {	
-				             this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power)+parseFloat(json.emeters[1].power)+parseFloat(json.emeters[2].power));
-				             this.totalPowerConsumption = Math.abs((parseFloat(json.emeters[0].total)+parseFloat(json.emeters[1].total)+parseFloat(json.emeters[2].total))/1000);
-			               this.voltage1 = Math.abs(((parseFloat(json.emeters[0].voltage)+parseFloat(json.emeters[1].voltage)+parseFloat(json.emeters[2].voltage))/3));
-				             this.ampere1 = Math.abs(((parseFloat(json.emeters[0].current)*this.pf0)
-					            +(parseFloat(json.emeters[1].current)*this.pf1)
-					            +(parseFloat(json.emeters[2].current)*this.pf2)));
-					          }
-							
+			try {
+				const json = JSON.parse(body);
+				// Power factor
+				if ((this.use_pf) && (this.use_em == false)) {
+					this.pf0 = parseFloat(json.emeters[0].pf);
+					this.pf1 = parseFloat(json.emeters[1].pf);
+					this.pf2 = parseFloat(json.emeters[2].pf);
+				} else {
+					this.pf0 = 1;
+					this.pf1 = 1;
+					this.pf2 = 1;
+				}
+				// Main measurement logic (preserved from original)
+				if (this.use_em) {
+					if (this.use_em_mode == 0) {
+						if (this.negative_handling_mode == 0) {
+							this.powerConsumption = (parseFloat(json.emeters[0].power) + parseFloat(json.emeters[1].power));
+							this.totalPowerConsumption = ((parseFloat(json.emeters[0].total) + parseFloat(json.emeters[1].total)) / 1000);
+							this.voltage1 = (((parseFloat(json.emeters[0].voltage) + parseFloat(json.emeters[1].voltage)) / 2));
+							this.ampere1 = ((this.powerConsumption / this.voltage1));
+							if (this.powerConsumption < 0) { this.powerConsumption = 0; }
+							if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0; }
+							if (this.voltage1 < 0) { this.voltage1 = 0; }
+							if (this.ampere1 < 0) { this.ampere1 = 0; }
+						} else if (this.negative_handling_mode == 1) {
+							this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power) + parseFloat(json.emeters[1].power));
+							this.totalPowerConsumption = Math.abs((parseFloat(json.emeters[0].total) + parseFloat(json.emeters[1].total)) / 1000);
+							this.voltage1 = Math.abs(((parseFloat(json.emeters[0].voltage) + parseFloat(json.emeters[1].voltage)) / 2));
+							this.ampere1 = Math.abs((this.powerConsumption / this.voltage1));
 						}
-							
-							
-					
-					if (this.debug_log) { this.log('Successful http response. [ voltage: ' + this.voltage1.toFixed(0) + 'V, current: ' + this.ampere1.toFixed(1) + 'A, consumption: ' + this.powerConsumption.toFixed(0) + 'W, total consumption: ' + this.totalPowerConsumption.toFixed(2) + 'kWh ]'); }
+					} else if (this.use_em_mode == 1) {
+						if (this.negative_handling_mode == 0) {
+							this.powerConsumption = (parseFloat(json.emeters[0].power));
+							this.totalPowerConsumption = (parseFloat(json.emeters[0].total) / 1000);
+							this.voltage1 = (parseFloat(json.emeters[0].voltage));
+							this.ampere1 = ((this.powerConsumption / this.voltage1));
+							if (this.powerConsumption < 0) { this.powerConsumption = 0; }
+							if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0; }
+							if (this.voltage1 < 0) { this.voltage1 = 0; }
+							if (this.ampere1 < 0) { this.ampere1 = 0; }
+						} else if (this.negative_handling_mode == 1) {
+							this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power));
+							this.totalPowerConsumption = Math.abs(parseFloat(json.emeters[0].total) / 1000);
+							this.voltage1 = Math.abs(parseFloat(json.emeters[0].voltage));
+							this.ampere1 = Math.abs((this.powerConsumption / this.voltage1));
+						}
+					} else if (this.use_em_mode == 2) {
+						if (this.negative_handling_mode == 0) {
+							this.powerConsumption = (parseFloat(json.emeters[1].power));
+							this.totalPowerConsumption = (parseFloat(json.emeters[1].total) / 1000);
+							this.voltage1 = (parseFloat(json.emeters[1].voltage));
+							this.ampere1 = ((this.powerConsumption / this.voltage1));
+							if (this.powerConsumption < 0) { this.powerConsumption = 0; }
+							if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0; }
+							if (this.voltage1 < 0) { this.voltage1 = 0; }
+							if (this.ampere1 < 0) { this.ampere1 = 0; }
+						} else if (this.negative_handling_mode == 1) {
+							this.powerConsumption = Math.abs(parseFloat(json.emeters[1].power));
+							this.totalPowerConsumption = Math.abs(parseFloat(json.emeters[1].total) / 1000);
+							this.voltage1 = Math.abs(parseFloat(json.emeters[1].voltage));
+							this.ampere1 = Math.abs((this.powerConsumption / this.voltage1));
+						}
+					}
+				} else {
+					if (this.negative_handling_mode == 0) {
+						this.powerConsumption = (parseFloat(json.emeters[0].power) + parseFloat(json.emeters[1].power) + parseFloat(json.emeters[2].power));
+						this.totalPowerConsumption = ((parseFloat(json.emeters[0].total) + parseFloat(json.emeters[1].total) + parseFloat(json.emeters[2].total)) / 1000);
+						this.voltage1 = (((parseFloat(json.emeters[0].voltage) + parseFloat(json.emeters[1].voltage) + parseFloat(json.emeters[2].voltage)) / 3));
+						this.ampere1 = (((parseFloat(json.emeters[0].current) * this.pf0)
+							+ (parseFloat(json.emeters[1].current) * this.pf1)
+							+ (parseFloat(json.emeters[2].current) * this.pf2)));
+						if (this.powerConsumption < 0) { this.powerConsumption = 0; }
+						if (this.totalPowerConsumption < 0) { this.totalPowerConsumption = 0; }
+						if (this.voltage1 < 0) { this.voltage1 = 0; }
+						if (this.ampere1 < 0) { this.ampere1 = 0; }
+					} else if (this.negative_handling_mode == 1) {
+						this.powerConsumption = Math.abs(parseFloat(json.emeters[0].power) + parseFloat(json.emeters[1].power) + parseFloat(json.emeters[2].power));
+						this.totalPowerConsumption = Math.abs((parseFloat(json.emeters[0].total) + parseFloat(json.emeters[1].total) + parseFloat(json.emeters[2].total)) / 1000);
+						this.voltage1 = Math.abs(((parseFloat(json.emeters[0].voltage) + parseFloat(json.emeters[1].voltage) + parseFloat(json.emeters[2].voltage)) / 3));
+						this.ampere1 = Math.abs(((parseFloat(json.emeters[0].current) * this.pf0)
+							+ (parseFloat(json.emeters[1].current) * this.pf1)
+							+ (parseFloat(json.emeters[2].current) * this.pf2)));
+					}
 				}
-				catch (parseErr) {
-					this.log('Error processing data: ' + parseErr.message);
-					error = parseErr;
+				if (this.debug_log) {
+					this.log('Successful http response. [ voltage: ' + this.voltage1.toFixed(0) + 'V, current: ' + this.ampere1.toFixed(1) + 'A, consumption: ' + this.powerConsumption.toFixed(0) + 'W, total consumption: ' + this.totalPowerConsumption.toFixed(2) + 'kWh ]');
 				}
+				// Update HomeKit characteristics
+				this.service.getCharacteristic(Characteristic.CurrentConsumption || Characteristic.ElectricCurrent)
+					.setValue(this.powerConsumption, undefined, undefined);
+				if (Characteristic.TotalConsumption) {
+					this.service.getCharacteristic(Characteristic.TotalConsumption)
+						.setValue(this.totalPowerConsumption, undefined, undefined);
+				}
+				if (Characteristic.Voltage) {
+					this.service.getCharacteristic(Characteristic.Voltage)
+						.setValue(this.voltage1, undefined, undefined);
+				}
+				if (Characteristic.Current) {
+					this.service.getCharacteristic(Characteristic.Current)
+						.setValue(this.ampere1, undefined, undefined);
+				}
+				this.service.getCharacteristic(Characteristic.On)
+					.setValue(this.powerConsumption > 0);
+				this.service.getCharacteristic(Characteristic.OutletInUse)
+					.setValue(this.powerConsumption > 0);
+				// FakeGato
+				this.historyService.addEntry({ time: Math.round(new Date().valueOf() / 1000), power: this.powerConsumption });
+			} catch (parseErr) {
+				this.log('Error processing data: ' + parseErr.message);
 			}
-			if (!error) {
-				
-				resolve(this.powerConsumption,this.totalPowerConsumption,this.voltage1,this.ampere1)
-			}
-			else {
-				reject(error);
-			}
-			this.waiting_response = false;
-		});
-	})
-	.then((value_current, value_total, value_voltage1, value_ampere1) => {
-		if (value_current != null) {
-				this.service.getCharacteristic(this._EvePowerConsumption).setValue(value_current, undefined, undefined);
-				//FakeGato
-				this.historyService.addEntry({time: Math.round(new Date().valueOf() / 1000), power: value_current});}
-		if (value_total != null) {
-				this.service.getCharacteristic(this._EveTotalConsumption).setValue(value_total, undefined, undefined);}
-		if (value_voltage1 != null) {
-				this.service.getCharacteristic(this._EveVoltage1).setValue(value_voltage1, undefined, undefined);}
-		if (value_ampere1 != null) {
-				this.service.getCharacteristic(this._EveAmpere1).setValue(value_ampere1, undefined, undefined);}
-		return true;
-	}, (error) => {
-		return error;
+		this.waiting_response = false;
 	});
-};
-
-EnergyMeter.prototype.getPowerConsumption = function (callback) {
-	callback(null, this.powerConsumption);
-};
-
-EnergyMeter.prototype.getTotalConsumption = function (callback) {
-	callback(null, this.totalPowerConsumption);
-};
-
-EnergyMeter.prototype.getVoltage1 = function (callback) {
-	callback(null, this.voltage1);
-};
-
-EnergyMeter.prototype.getAmpere1 = function (callback) {
-	callback(null, this.ampere1);
-};
-
-EnergyMeter.prototype.getServices = function () {
-	this.log("getServices: " + this.name);
-	if (this.update_interval > 0) {
-		this.timer = setInterval(this.updateState.bind(this), this.update_interval);
-	}
-	return [this.informationService, this.service, this.historyService];
 };
